0056919c395c0aff22163eabe0e08314a84cffe4
Mario <mariokozul24@gmail.com>
2023-09-17 18:37:57 +0200

    added logic for deleting parts and removing images from storage after deletion

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import morgan from 'morgan';
 import { connectToDb, getDb } from './db.js';
 import multer from 'multer';
 import path from 'path';
+import { ObjectId } from 'mongodb';
+import {unlink} from 'fs';
 let CUR_FILE_NAME;
 const app = express();
 app.use(express.json());
@@ -97,7 +99,7 @@ app.post('/', upload.single('image') ,async (req, res) => {
     let newItem = req.body;
     newItem.file = CUR_FILE_NAME;
     
-    console.log(newItem);
+    //console.log(newItem);
     try{
         const result = await db.collection('products')
             .insertOne(newItem)
@@ -107,6 +109,42 @@ app.post('/', upload.single('image') ,async (req, res) => {
         res.status(500).redirect('/404');
     }
 })
+
+app.get('/:id', async (req,res) => {
+    try {
+        if (ObjectId.isValid(req.params.id)) {
+            const part = await db.collection('products')
+                .findOne({_id: new ObjectId(req.params.id)})
+            res.status(200).render('categories/partDetails', {title: 'Details', data: part})
+        }
+        else res.status(500).render('404', {title: '404'});
+    } catch (err) {
+        console.log(err);
+        res.status(500).render('404');
+    }
+})
+
+app.delete('/:id', async (req,res) => {
+    try{
+        if(ObjectId.isValid(req.params.id)){
+            const part = await db.collection('products')
+                .findOne({_id: new ObjectId(req.params.id)})
+
+            const result = await db.collection('products')
+                .deleteOne({_id: new ObjectId(req.params.id)})
+            res.status(200).json({redirect: '/'});
+            console.log(result, 'peder');
+            unlink(`public/${part.file}`, (err) => {
+                console.log(err);
+            })
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).redirect('/404');
+    }
+})
+
+
 //404
 app.use((req,res) => {
     res.status(404).render('404', {title: '404'});
