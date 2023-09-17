ebd65ab53e5eb5f20d5bf195e4796b2f26a62545
Mario <mariokozul24@gmail.com>
2023-09-17 14:39:50 +0200

    added logic for multer and getting items from database

--- app.js
+++ app.js
@@ -2,10 +2,27 @@ import express from 'express';
 import { render } from 'ejs';
 import morgan from 'morgan';
 import { connectToDb, getDb } from './db.js';
-
+import multer from 'multer';
+import path from 'path';
+let CUR_FILE_NAME;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+//multer conf
+
+const storage = multer.diskStorage({
+    destination: (req,file, cb) => {
+        cb(null, 'public')
+    },
+    filename: (req, file, cb) => {
+        const name = Date.now() + path.extname(file.originalname);
+        CUR_FILE_NAME = name;
+        cb(null, name);
+    }
+})
+
+const upload = multer({storage: storage});
 
 
 //db and express connection
@@ -32,8 +49,15 @@ app.use(morgan('dev'));
 
 
 //routes
-app.get('/', (req, res) => {
-    res.render('categories/index',{title: 'Car parts'});
+app.get('/', async (req, res) => {
+    try {
+        const parts = await db.collection('products')
+            .find()
+            .toArray();
+        res.render('categories/index',{title: 'Car parts', data: parts});
+    } catch (err) {
+        res.status(500).render('404', {title: '404'});
+    }
 })
 
 app.get('/engine', (req, res) => {
@@ -56,6 +80,20 @@ app.get('/new', (req, res) => {
     res.render('categories/newPart',{title: 'New part'});
 })
 
+app.post('/', upload.single('image') ,async (req, res) => {
+    let newItem = req.body;
+    newItem.file = CUR_FILE_NAME;
+    
+    console.log(newItem);
+    try{
+        const result = await db.collection('products')
+            .insertOne(newItem)
+        res.status(200).redirect('/');
+    } catch (err) {
+        console.log(err);
+        res.status(500).redirect('/404');
+    }
+})
 //404
 app.use((req,res) => {
     res.status(404).render('404', {title: '404'});
